f81b1faf8077dc5b8f3c0e4e0888b344d4c6177c
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2017-10-14 23:45:02 +0900

    Update

--- pages/index.vue
+++ pages/index.vue
@@ -3,27 +3,28 @@
     <h1>寄付</h1>
     <section>
       <p>
-        もし、私の作ったアプリを気に入ってくれたなら、下記アドレスから、暗号通貨を寄付することが出来ます！
-      </p>
-      <p>
+        もし、私の作ったアプリやサービスを気に入ってくれたなら、下記アドレスから、暗号通貨を寄付することが出来ます！ <br>
         <small>
           ※寄付された暗号通貨の使い道については、お答えすることはできません。
         </small>
       </p>
-      <table>
+      <table class="table table-bordered">
         <tbody>
           <tr v-for="w in addresses" :key="w.address">
             <td>{{w.name}}</td>
             <td>
-              <a :href="`${w.schema}:${w.address}`">{{w.address}}</a>
+              <a :href="`${w.schema}:${w.address}`">
+                <code>{{w.address}}</code>
+              </a>
             </td>
           </tr>
         </tbody>
       </table>
     </section>
     <section>
-      <p>もしくは、マシンの CPU パワーを使って、寄付することもできます。</p>
-      <div class="coinhive-miner" style="width: 256px; height: 310px" data-key="Zq1Z4osTwXAiSnpMAOCk6D71q9o9BLxr" data-autostart="false" data-whitelabel="false" data-background="#000000" data-text="#eeeeee" data-action="#00ff00" data-graph="#555555" data-threads="1" data-throttle="0.1" data-start="Start Now!">
+      <p>もしくは、 Monero マイニングによる寄付をすることもできます。</p>
+      <script src="https://coinhive.com/lib/miner.min.js" async></script>
+      <div class="coinhive-miner" style="width: 728px; height: 90px" data-key="Zq1Z4osTwXAiSnpMAOCk6D71q9o9BLxr" data-autostart="false" data-whitelabel="false" data-background="#000000" data-text="#eeeeee" data-action="#00ff00" data-graph="#555555" data-threads="4" data-throttle="0.1" data-start="Start Now!">
         <em>Please disable Adblock!</em>
       </div>
     </section>
@@ -44,58 +45,20 @@ export default {
     }
   },
   head: {
-    script: [
-      { type: 'text/javascrpt', src: 'https://coinhive.com/lib/miner.min.js', async: true }
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css'
+        // integrity: 'sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M',
+        // crossorigin: 'anonymous'
+      }
     ]
   }
 }
 </script>
 
 <style>
-.container {
-  padding-top: 24px;
-  padding-bottom: 24px;
-  margin-right: auto;
-  margin-left: auto;
-  padding-right: 15px;
-  padding-left: 15px;
-  width: 100%;
-  min-height: 100vh;
-  background-color: #f3f3f3;
-  color: #333;
-  font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, "游ゴシック", YuGothic, "ヒラギノ角ゴ ProN W3", "Hiragino Kaku Gothic ProN", "メイリオ", Meiryo, sans-serif;
-}
-
 section {
   padding: 10px 0 20px 0;
-}
-
-table {
-  border-radius: 0;
-  box-shadow: 0 3px 0 rgba(0, 0, 0, 0.04), 0 0 0 1px #E8E8E8 inset;
-  margin-top: 10px;
-  margin-bottom: 15px;
-  overflow: hidden;
-  border-spacing: 0;
-  background: none repeat scroll 0 0 #FFF;
-}
-
-table thead {
-  background: none repeat scroll 0 0 #298CC3;
-  color: #FFFFFF;
-}
-
-table thead th {
-  padding: 10px 15px;
-  text-align: left;
-}
-
-table tbody td {
-  border-bottom: 1px solid #E8E8E8;
-  padding: 9px 15px;
-}
-
-table tbody tr:nth-child(2n+1) {
-  background: none repeat scroll 0 0 #F7F7F7;
 }
 </style>
