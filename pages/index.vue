9fce0fd69e9743c2b8e94716a26285945326c664
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2017-12-30 18:38:09 +0900

    Add addresses

--- pages/index.vue
+++ pages/index.vue
@@ -5,21 +5,50 @@
       <p>
         もし、私の作ったアプリやサービスを気に入ってくれたなら、下記アドレスから、暗号通貨を寄付することが出来ます！ <br>
         <small>
-          ※寄付された暗号通貨の使い道については、お答えすることはできません。
+          ※寄付された暗号通貨の使い道については、お答えすることはできません。 <br>
+          ※単純に投げ銭でも嬉しいです。
         </small>
       </p>
-      <table class="table table-bordered">
-        <tbody>
-          <tr v-for="w in addresses" :key="w.address">
-            <td>{{w.name}}</td>
-            <td>
-              <a :href="`${w.schema}:${w.address}`">
+      <div class="d-none d-sm-block">
+        <div class="row">
+          <div class="col-md-6">
+            <table class="table table-bordered">
+              <tbody>
+                <tr v-for="w in addresses.filter((el, idx, arr) => idx % 2 == 0)" :key="w.address">
+                  <th>{{w.name}}</th>
+                  <td>
+                    <code>{{w.address}}</code>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="col-md-6">
+            <table class="table table-bordered">
+              <tbody>
+                <tr v-for="w in addresses.filter((el, idx, arr) => idx % 2 == 1)" :key="w.address">
+                  <th>{{w.name}}</th>
+                  <td>
+                    <code>{{w.address}}</code>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="d-block d-sm-none">
+        <table style="table-layout: fixed;" class="table table-bordered table-responsive">
+          <tbody>
+            <tr v-for="w in addresses" :key="w.address">
+              <th style="min-width: 150px">{{w.name}}</th>
+              <td>
                 <code>{{w.address}}</code>
-              </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </section>
     <section>
       <p>もしくは、 Monero マイニングによる寄付をすることもできます。</p>
@@ -32,71 +61,120 @@
 </template>
 
 <script>
+import sortBy from "lodash.sortby";
+
 export default {
   data: () => {
     return {
-      addresses: [
-        {
-          name: "Bitcoin",
-          address: "1Gvd71AtuDRfkLkfpXcZtAantwJUjYv3tN",
-          schema: "bitcoin"
-        },
-        {
-          name: "Bitcoin Cash",
-          address: "32ctqTga55CcV84kY9WHUMKZwjSJBMGJfH",
-          schema: ""
-        },
-        {
-          name: "BitCore",
-          address: "15PseUcuyrZ1uRTJHCCeDBkj9P5dFdnMhP",
-          schema: "bitcore"
-        },
-        {
-          name: "BitZeny",
-          address: "ZbVpLYsDGGmNvKUzx7YRQm3MWpENQ19DNg",
-          schema: "bitzeny"
-        },
-        {
-          name: "Blackcoin",
-          address: "BSC9NPfCrFrRkfFiFzbmZ3PAhuX5GSEKQ7",
-          schema: "blackcoin"
-        },
-        {
-          name: "Dash",
-          address: "XqAr2j1pTj34Yc9nLLsLCyFeEH9o32ghgf",
-          schema: "dash"
-        },
-        {
-          name: "Dogecoin",
-          address: "D7Ydsu2TWWjdSaV1zfg9y8Y9Z4KJzh4ucp",
-          schema: "dogecoin"
-        },
-        {
-          name: "Litecoin",
-          address: "LVUiYNgvaynAvCYN45tjojGSfuWd2BSfUj",
-          schema: "litecoin"
-        },
-        {
-          name: "Monacoin",
-          address: "MC4x87u1ffmhsRhHof1sHz8UAtNtKCTQut",
-          schema: "monacoin"
-        },
-        {
-          name: "Peercoin",
-          address: "PG8Ja96Th6mpwjDhTTQcmokzfbR5ZeZmk6",
-          schema: "peercoin"
-        },
-        {
-          name: "Primecoin",
-          address: "AYMwtLDjxMmWsUctYH7AX27bFkKn5Dby3d",
-          schema: "primecoin"
-        },
-        {
-          name: "XP",
-          address: "XZvrWCPeHtzBAUx2P3fcwGCB5kSsfiXMc6",
-          schema: "xp"
-        }
-      ]
+      addresses: sortBy(
+        [
+          {
+            name: "Bitcoin",
+            address: "1Gvd71AtuDRfkLkfpXcZtAantwJUjYv3tN",
+            schema: "bitcoin"
+          },
+          {
+            name: "Bitcoin Cash",
+            address: "32ctqTga55CcV84kY9WHUMKZwjSJBMGJfH",
+            schema: ""
+          },
+          {
+            name: "BitCore",
+            address: "15PseUcuyrZ1uRTJHCCeDBkj9P5dFdnMhP",
+            schema: "bitcore"
+          },
+          {
+            name: "BitZeny",
+            address: "ZbVpLYsDGGmNvKUzx7YRQm3MWpENQ19DNg",
+            schema: "bitzeny"
+          },
+          {
+            name: "Blackcoin",
+            address: "BSC9NPfCrFrRkfFiFzbmZ3PAhuX5GSEKQ7",
+            schema: "blackcoin"
+          },
+          {
+            name: "Dash",
+            address: "XqAr2j1pTj34Yc9nLLsLCyFeEH9o32ghgf",
+            schema: "dash"
+          },
+          {
+            name: "Dogecoin",
+            address: "D7Ydsu2TWWjdSaV1zfg9y8Y9Z4KJzh4ucp",
+            schema: "dogecoin"
+          },
+          {
+            name: "Litecoin",
+            address: "LVUiYNgvaynAvCYN45tjojGSfuWd2BSfUj",
+            schema: "litecoin"
+          },
+          {
+            name: "Monacoin",
+            address: "MC4x87u1ffmhsRhHof1sHz8UAtNtKCTQut",
+            schema: "monacoin"
+          },
+          {
+            name: "Peercoin",
+            address: "PG8Ja96Th6mpwjDhTTQcmokzfbR5ZeZmk6",
+            schema: "peercoin"
+          },
+          {
+            name: "Primecoin",
+            address: "AYMwtLDjxMmWsUctYH7AX27bFkKn5Dby3d",
+            schema: "primecoin"
+          },
+          {
+            name: "XP",
+            address: "XZvrWCPeHtzBAUx2P3fcwGCB5kSsfiXMc6",
+            schema: "xp"
+          },
+          {
+            name: "Ethereum",
+            address: "0xDc48d5AF8dCa1284F8770553E6e01beeE510535D"
+          },
+          {
+            name: "Ripple",
+            address: "rUpRPeEwjkHwU4wyZKeFLRQQvCguxZP4UG"
+          },
+          {
+            name: "Fujicoin",
+            address: "FbSYmbpK1ZyrE3zUDTGcjf1sUDpq1DU7vV"
+          },
+          {
+            name: "Nekonium",
+            address: "0xAb18ea682305E4264C6F40Fb2F61a37FB847201f"
+          },
+          {
+            name: "NEM",
+            address: "NDXD3H-ZJVPEI-JDWA7Q-56DUKJ-3YYQZV-QHX7DL-GMDV"
+          },
+          {
+            name: "Neo",
+            address: "ALaiMeM3Je2ehPpwvBMuNGHaaYBWFe7de3"
+          },
+          {
+            name: "ZCash",
+            address: "t1MyEJt3HVaLAxm4tU1a8w5K1wWBLZXn79Q"
+          },
+          {
+            name: "Ethereum Classic",
+            address: "0xfd0859b6f00bee9777511c96cd3e86418390a99d"
+          },
+          {
+            name: "Angur",
+            address: "0x423a129b99d3a8be088dcec89878ca8dba67a775"
+          },
+          {
+            name: "decred",
+            address: "DskczKwMVSKHcwwVSLMPrJnRAPzQiqEW19Y"
+          },
+          {
+            name: "NXT",
+            address: "NXT-6UKG-V7TL-TA7C-4K3KP"
+          }
+        ],
+        w => w.name.toLocaleLowerCase()
+      )
     };
   },
   head: {
